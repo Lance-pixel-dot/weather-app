9b329bbc072f81151f1bda8141dbb19141c68c41
Renier Lawrence Tuico <renierlawrencetuico143@gmail.com>
2025-05-06 19:07:15 +0800

    Create temperature toggle between Celsius and Fahrenheit.

--- src/get-weather.js
+++ src/get-weather.js
@@ -1,3 +1,5 @@
+import { toggleTempFormat } from "./toggle-temperature";
+
 const contentDiv = document.querySelector('#content');
 
 async function getLocationWeather(location) {
@@ -7,24 +9,66 @@ async function getLocationWeather(location) {
         mode: 'cors'
         });
         const weather = await response.json();
+
         console.log(weather);
         contentDiv.textContent = '';
+
         const weatherStatus = document.createElement('div');
         weatherStatus.textContent = weather.currentConditions.conditions;
+
         const weatherTemp = document.createElement('div');
-        weatherTemp.textContent = `${weather.currentConditions.temp}°Celsius`;
+        weatherTemp.textContent = `${weather.currentConditions.temp}° Celsius`;
+
         const convertTempButton = document.createElement('button');
         convertTempButton.className = 'convert-temp';
-        convertTempButton.textContent = 'Convert to Fahrenheit';
+        
+        const getLocalTempFormat = JSON.parse(localStorage.getItem('tempFormat'));
+
+        if(getLocalTempFormat == null){
+            const currentTempFormat = 'Celsius';
+            convertTempButton.textContent = `Convert to Fahrenheit`;
+            localStorage.setItem('tempFormat', JSON.stringify(currentTempFormat))
+        }else if(getLocalTempFormat == 'Celsius'){
+            weatherTemp.textContent = `${weather.currentConditions.temp}° Celsius`;
+            convertTempButton.textContent = `Convert to Fahrenheit`;
+        }else if(getLocalTempFormat == 'Fahrenheit'){
+            const toFahrenheit = (weather.currentConditions.temp * 9/5) + 32;
+            weatherTemp.textContent = `${toFahrenheit}° Fahrenheit`;
+            convertTempButton.textContent = `Convert to Celsius`;
+        }
+
+        convertTempButton.addEventListener('click', () => {
+            const getLocalTempFormat = JSON.parse(localStorage.getItem('tempFormat'));
+
+            if(getLocalTempFormat == 'Celsius'){
+
+                const toFahrenheit = (weather.currentConditions.temp * 9/5) + 32;
+                weatherTemp.textContent = `${toFahrenheit}° Fahrenheit`;
+                const currentTempFormat = 'Fahrenheit';
+                localStorage.setItem('tempFormat', JSON.stringify(currentTempFormat));
+                convertTempButton.textContent = `Convert to ${getLocalTempFormat}`;
+
+            }else if(getLocalTempFormat == 'Fahrenheit'){
+
+                weatherTemp.textContent = `${weather.currentConditions.temp}° Celsius`;
+                const currentTempFormat = 'Celsius';
+                localStorage.setItem('tempFormat', JSON.stringify(currentTempFormat));
+                convertTempButton.textContent = `Convert to ${getLocalTempFormat}`;
+
+            }
+        })
+
         contentDiv.appendChild(weatherStatus);
         contentDiv.appendChild(weatherTemp);
         contentDiv.appendChild(convertTempButton);
+
         const weatherImg = document.createElement('img');
         import(`./svg/${weather.currentConditions.icon}.svg`)
             .then((icon) => {
                 weatherImg.src = icon.default;
             })
         weatherStatus.appendChild(weatherImg);
+
     } catch{
         contentDiv.textContent = "Error: couldn't fetch data from the server";
     }
--- src/toggle-temperature.js
+++ src/toggle-temperature.js
@@ -1,5 +1,10 @@
-function toggleTempFormat() {
-    
+function toggleTempFormat(currentTemp) {
+    const currentTempFormat = document.querySelector('.convert-temp');
+    if(currentTempFormat.textContent == 'Convert to Fahrenheit'){
+        currentTempFormat.textContent = 'Convert to Celsius';
+    }else if(currentTempFormat.textContent == 'Convert to Celsius'){
+        currentTempFormat.textContent = 'Convert to Fahrenheit';
+    }
 }
 
-// export { getLocationWeather };
+export { toggleTempFormat };
